94b6e8c475270e2b257c843e687e280fc48ddb13
Julien Onorato <julien.onorato@epitech.eu>
2025-06-07 12:48:47 +0200

    feat(battle-moves): buff LeafBlades

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -4527,7 +4527,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     [MOVE_LEAF_BLADE] =
     {
         .effect = EFFECT_HIGH_CRITICAL,
-        .power = 70,
+        .power = 80,
         .type = TYPE_GRASS,
         .accuracy = 100,
         .pp = 15,
